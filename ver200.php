1637017fe93034fce9219c7cfbf93c74719dea33
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-03-18 16:13:51 -0600

    Create ver200.php

---
+++ ver200.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Hice una utilidad para poder verificar que los dominios estén 
+online, es decir, respuesta 200 correcta.";
+echo "<br><a href='https://github.com/AlfonsoOrozcoAguilarnoNDA/lemkotir/blob/main/ver200.php' 
+target='_blank'>Ver código en GitHub</a>";
+?>
